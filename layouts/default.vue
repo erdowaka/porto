b932be5eca69b80d02c472206b10792b7b013d6f
Erdo Waka Septyano <32562893+erdowaka@users.noreply.github.com>
2022-07-08 17:23:16 +0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -60,7 +60,7 @@
     <div class="footer-content">
       <div class="row">
         <div class="col-lg-8 footer-left">
-          <p class="m-0">{{data.main.shortname}} © Copyright 2021. Developed by <a href="https://.com/" target="_blank">Erdo Waka Septyano</a></p>
+          <p class="m-0">{{data.main.shortname}} © Copyright 2022. Developed by <a href="https://.com/" target="_blank">Erdo Waka Septyano</a></p>
         </div>
         <div class="col-lg-4 footer-right text-lg-right text-center mt-lg-0 mt-3">
           <ul class="social m-0 p-0">
